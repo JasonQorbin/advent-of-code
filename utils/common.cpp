71036fe4f235bdfed5b78b1144e4213240732a63
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-01-26 08:43:22 +0200

    Added parenthessis in common.cpp to remove compilation warning.

--- utils/common.cpp
+++ utils/common.cpp
@@ -10,7 +10,7 @@
 /// with a teset line after it.
 bool checkArgs(int argc, char* argv[]) {
     using namespace std;
-    if (argc < 2 || strcmp(argv[1],"-l") == 0 && argc < 3) {
+    if (argc < 2 || ( strcmp(argv[1],"-l") == 0 && argc < 3 )) {
         cout << "Usage: " << argv[0] << "<inputFile.txt>" << endl;
         cout << "Usage: " << argv[0] << "line-of-text-to-test" << endl;
         return false;
